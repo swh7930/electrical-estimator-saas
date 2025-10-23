7944585899918f721299dc0163ce4f8b58af2d61
swh7930 <swh7930@yahoo.com>
2025-10-23 07:46:36 -0500

    fix(migrations): import postgresql dialect in 3b.4 mail migration

--- migrations/versions/d5328dd90b4c_s3_03b_4_mail_production_plan.py
+++ migrations/versions/d5328dd90b4c_s3_03b_4_mail_production_plan.py
@@ -7,7 +7,7 @@ Create Date: 2025-10-23 07:12:39.409149
 """
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'd5328dd90b4c'
